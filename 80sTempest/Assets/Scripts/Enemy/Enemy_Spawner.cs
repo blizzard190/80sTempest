62c15c186c6e76d7743b32c17f18ecb59b88c078
Feaxxae <duncan.westerdijk@quicknet.nl>
2019-09-03 08:09:41 +0100

    Enemy
    Enemy AI created
    Enemy spawner createded
    Leave implementing it to me.

---
+++ 80sTempest/Assets/Scripts/Enemy/Enemy_Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour
+{   
+    private int cooldown = 60;
+
+
+    public GameObject _enemy;
+
+
+    void Update()
+    {
+        if (cooldown == 0)
+        {
+            Instantiate(_enemy, this.transform);
+            cooldown = 60;
+        }
+        cooldown--;
+    }
+}
