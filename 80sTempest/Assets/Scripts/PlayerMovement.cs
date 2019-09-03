e76a876cc0fa059b973119d6946f4dcb6e4865d9
max <1223max@gmail.com>
2019-09-03 09:18:03 +0200

    updated player

---
+++ 80sTempest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int Pos;
+    private Transform newPos;
+    public Transform center;
+    private void Update()
+    {
+        transform.LookAt(center);
+
+        if (Input.GetKeyDown("left"))
+        {
+            GetPosL();
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            GetPosR();
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+
+    }
+
+    void GetPosL()
+    {
+        if (Pos == 16)
+        {
+            Pos = 0;
+        }
+        else
+        {
+            sides[Pos - 1] = newPos;
+        }
+    }
+
+    void GetPosR()
+    {
+        if (Pos == 0)
+        {
+            Pos = 16;
+        }
+        else
+        {
+            sides[Pos + 1] = newPos;
+        }
+    }
+
+
+}
