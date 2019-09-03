357d1c094d7247eaab76cbf243cb483c6020d841
blizzard190 <22256@ma-web.nl>
2019-09-03 09:07:17 +0200

    Array
    Created basic array's + prefabs

---
+++ 80sTempest/Assets/Scripts/Side.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Side : MonoBehaviour
+{
+    public GameObject[] lines;
+    public Transform spawn;
+}
---
+++ 80sTempest/Assets/Scripts/SideManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideManager : MonoBehaviour
+{
+    public GameObject[] sides;
+}
+
